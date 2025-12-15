d728588d3addaea9b4ec0bf60956eb877c7f299d
Partha-Dey001 <deyp9187@gmail.com>
2025-12-15 09:16:17 +0530

    feat: Initial project structure

---
+++ src/main/java/com/partha/expensemanager/controller/HomeController.java
@@ -0,0 +1,18 @@
+package com.partha.expensemanager.controller;
+
+
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping({"/statur", "/health"})
+public class HomeController {
+
+
+    @GetMapping
+    public String healthCheck() {
+        return "Application is running!";
+    }
+}
