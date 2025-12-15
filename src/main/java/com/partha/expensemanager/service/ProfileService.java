0fee79194b04b00aa1696b7c8e8ff9f6538143ed
Partha-Dey001 <deyp9187@gmail.com>
2025-12-15 22:13:59 +0530

    feat: user registration (configured email placeholder)

---
+++ src/main/java/com/partha/expensemanager/service/ProfileService.java
@@ -0,0 +1,46 @@
+package com.partha.expensemanager.service;
+
+import com.partha.expensemanager.dto.ProfileDTO;
+import com.partha.expensemanager.entity.ProfileEntity;
+import com.partha.expensemanager.repository.ProfileRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class ProfileService {
+
+    private final ProfileRepository profileRepository;
+
+    public ProfileDTO registerProfile(ProfileDTO profileDTO) {
+        ProfileEntity newProfile = toEntity(profileDTO);
+        newProfile.setActivationToken(UUID.randomUUID().toString());
+        newProfile = profileRepository.save(newProfile);
+        return toDTO(newProfile);
+    }
+
+    public ProfileEntity toEntity(ProfileDTO profileDTO) {
+        return ProfileEntity.builder()
+                .id(profileDTO.getId())
+                .fullName(profileDTO.getFullName())
+                .email(profileDTO.getEmail())
+                .passWord(profileDTO.getPassWord())
+                .profileImageUrl(profileDTO.getProfileImageUrl())
+                .createdAt(profileDTO.getCreatedAt())
+                .updatedAt(profileDTO.getUpdatedAt())
+                .build();
+    }
+
+    public ProfileDTO toDTO(ProfileEntity profileEntity) {
+        return ProfileDTO.builder()
+                .id(profileEntity.getId())
+                .fullName(profileEntity.getFullName())
+                .email(profileEntity.getEmail())
+                .profileImageUrl(profileEntity.getProfileImageUrl())
+                .createdAt(profileEntity.getCreatedAt())
+                .updatedAt(profileEntity.getUpdatedAt())
+                .build();
+    }
+}
